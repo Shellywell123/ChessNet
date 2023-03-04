e448ecb6c40ba7d0b88b3bde8c24c1165b3d93d2
Shellywell123 <benjamin@shellswell.com>
2023-03-04 19:45:20 +0000

    experiment

---
+++ go/entities.go
@@ -0,0 +1,47 @@
+// THIS SHOULD:
+// - Entity:(own contained functions)
+// - Recieve chess notation FROM a chess engine - is this needed if we can passs the board
+
+package entities
+
+import (
+	"io"
+	"strings"
+	"os"
+	"log"
+	
+	u "github.com/Shellywell123/ChessNut-Pie/go/untilities"
+)
+
+// - Start a chess engine
+func startEngine() {
+	
+}
+
+// - Send chess notation to a chess engine - gnuchess already does this
+func sendChessMoveToEngine(GNUMOVE string, stdin io.WriteCloser) {
+	u.writeStdOut(strings.Split(GNUMOVE, ":")[1]+"\n", stdin)
+}
+
+// - Load a saved game of chess (PGN/FEM)
+func loadGameToEngine(GAMEFILE string, stdin io.WriteCloser) {
+	if u.fileExists(GAMEFILE) {
+		u.writeStdOut("pgnload "+GAMEFILE+"\n", stdin)
+	} else {
+		emptyFile, err := os.Create(GAMEFILE)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(emptyFile)
+		emptyFile.Close()
+	}
+}
+
+// - Save a game of chess (note - this does not mean persistence like on disk) (PGN/FEM)
+func saveGameFromEngine(GAMEFILE string, stdin io.WriteCloser) {
+	u.writeStdOut("pgnsave "+GAMEFILE+"\n", stdin)
+}
+
+func quitGameEngine(stdin io.WriteCloser) {
+	u.writeStdOut("quit\n", stdin)
+}
---
+++ go/notes/Environment-Variables.md
@@ -0,0 +1,41 @@
+# Environment variables
+
+# Helpful commands
+
+## System wide Environment Variables
+You want to be sure that your `$GOPATH/bin` and `$GOROOT/bin` existing in your `PATH` environment variables. In linux your 
+
+### Show your system wide paths
+In Linux (`PATH` = string of `:` delimted paths)
+```bash
+env | grep PATH
+```
+In Windows CMD (`Path` = string of `;` delimted paths)
+```bash
+SET | findstr Path
+```
+
+### Adding your Go paths to system wide paths
+In Linux
+```bash
+export GOROOT = /ussr/local/go
+export GOPAT = $HOME/workspace
+PATH = PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+```
+
+In Windows CMD
+```bash
+TODO
+```
+
+# Go related variables
+List out all your go related environment variables
+```bash
+go env
+```
+
+# Paths
+
+There are two main paths to worry about when using go
+ - `GOROOT`, this is the path to your go binary typical location is `/usr/local/go`
+ - `GOPATH` this is the path to your "workspace" (where your go projects live)
