ca9ebe5301926d9f054d74b9c119695527f492f7
Ben Shellswell <benjamin.shellswell@unity3d.com>
2023-03-07 20:57:25 +0000

    tests for trialling github actions

--- chessnet/main_test.go
+++ chessnet/main_test.go
@@ -10,3 +10,15 @@ func Test_test1(t *testing.T) {
 	equalThis := true
 	require.Equal(t, doesThis, equalThis)
 }
+
+func Test_test2(t *testing.T) {
+	doesThis := true
+	equalThis := true
+	require.Equal(t, doesThis, equalThis)
+}
+
+func Test_test3(t *testing.T) {
+	doesThis := true
+	equalThis := true
+	require.Equal(t, doesThis, equalThis)
+}
